ce56cea992abe3e3b1d671f50520d4798927d904
zmendax <2201382458@qq.com>
2025-07-13 16:57:50 +0800

    hugo extended

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # 下载 Hugo Extended 版本（Linux AMD64 示例，注意版本号换成你需要的）
-wget https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
+curl -L -o hugo.tar.gz https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
 
 tar -xzf hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
 chmod +x hugo
